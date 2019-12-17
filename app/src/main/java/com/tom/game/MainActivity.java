7fe7de97158d7a4398ae5aa0f899bdc4c75bb26d
miia <miia@com.tw>
2019-12-17 16:25:17 +0800

    1217-2

--- app/src/main/java/com/tom/game/MainActivity.java
+++ app/src/main/java/com/tom/game/MainActivity.java
@@ -13,13 +13,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bLeft = findViewById(R.id.arrow_left);
-        bLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        
 
         findViews();
     }
